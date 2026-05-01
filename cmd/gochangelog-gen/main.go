5e795a36e5bf21ac75dbc966cc111f7679681d5d
ESousa97 <xfuuuuuuuuury2018@gmail.com>
2026-05-01 00:34:26 -0300

    refactor: reduce cyclomatic complexity of main function
    - Extract flag parsing into parseFlags()
    - Extract logger setup into setupLogger()
    - Extract tag map resolution into getTagMap()
    - Use options struct to pass parameters to run()
    - Improve overall code readability and maintainability

--- cmd/gochangelog-gen/main.go
+++ cmd/gochangelog-gen/main.go
@@ -21,48 +21,65 @@ var (
 	buildTime = "unknown"
 )
 
+type options struct {
+	targetVersion string
+	templatePath  string
+	outputPath    string
+	githubRelease bool
+}
+
 func main() {
-	logger := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
-		Level: slog.LevelInfo,
-	}))
-	slog.SetDefault(logger)
+	setupLogger()
 
-	versionFlag := flag.String("version", "vUnreleased", "Version to display in the changelog title")
-	templateFlag := flag.String("template", "", "Path to a custom Markdown template file (.tmpl)")
-	githubReleaseFlag := flag.Bool("github-release", false, "Create a draft release on GitHub")
-	outputFlag := flag.String("output", "CHANGELOG_PENDING.md", "Output file path for the generated changelog")
-	showVersion := flag.Bool("v", false, "Show application version and exit")
-	flag.Parse()
-
-	if *showVersion {
-		fmt.Printf("gochangelog-gen %s (built at %s)\n", version, buildTime)
+	opts, shouldExit := parseFlags()
+	if shouldExit {
 		return
 	}
 
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
 
-	if err := run(ctx, *versionFlag, *templateFlag, *outputFlag, *githubReleaseFlag); err != nil {
+	if err := run(ctx, opts); err != nil {
 		slog.Error("execution failed", slog.String("error", err.Error()))
-		stop()
 		os.Exit(1)
 	}
-	stop()
 }
 
-func run(ctx context.Context, targetVersion, templatePath, outputPath string, githubRelease bool) error {
+func setupLogger() {
+	logger := slog.New(slog.NewJSONHandler(os.Stderr, &slog.HandlerOptions{
+		Level: slog.LevelInfo,
+	}))
+	slog.SetDefault(logger)
+}
+
+func parseFlags() (options, bool) {
+	var opts options
+	var showVersion bool
+
+	flag.StringVar(&opts.targetVersion, "version", "vUnreleased", "Version to display in the changelog title")
+	flag.StringVar(&opts.templatePath, "template", "", "Path to a custom Markdown template file (.tmpl)")
+	flag.BoolVar(&opts.githubRelease, "github-release", false, "Create a draft release on GitHub")
+	flag.StringVar(&opts.outputPath, "output", "CHANGELOG_PENDING.md", "Output file path for the generated changelog")
+	flag.BoolVar(&showVersion, "v", false, "Show application version and exit")
+	flag.Parse()
+
+	if showVersion {
+		fmt.Printf("gochangelog-gen %s (built at %s)\n", version, buildTime)
+		return opts, true
+	}
+
+	return opts, false
+}
+
+func run(ctx context.Context, opts options) error {
 	repo, err := gitrepo.Open(".")
 	if err != nil {
 		return fmt.Errorf("opening repository: %w", err)
 	}
 
-	tagMap, err := repo.GetTagMap()
+	strTagMap, err := getTagMap(repo)
 	if err != nil {
-		return fmt.Errorf("getting tags: %w", err)
-	}
-
-	strTagMap := make(map[string]string, len(tagMap))
-	for hash, tag := range tagMap {
-		strTagMap[hash.String()] = tag
+		return err
 	}
 
 	commitsIter, err := repo.GetCommits()
@@ -70,29 +87,40 @@ func run(ctx context.Context, targetVersion, templatePath, outputPath string, gi
 		return fmt.Errorf("getting commits: %w", err)
 	}
 
-	generator, err := changelog.NewGenerator(commitsIter, strTagMap, templatePath)
+	generator, err := changelog.NewGenerator(commitsIter, strTagMap, opts.templatePath)
 	if err != nil {
 		return fmt.Errorf("creating generator: %w", err)
 	}
 
-	content, nextVersion, err := generator.Generate(targetVersion)
+	content, nextVersion, err := generator.Generate(opts.targetVersion)
 	if err != nil {
 		return fmt.Errorf("generating changelog: %w", err)
 	}
 
-	if err := os.WriteFile(outputPath, []byte(content), 0o600); err != nil {
-		return fmt.Errorf("writing changelog file %s: %w", outputPath, err)
+	if err := os.WriteFile(opts.outputPath, []byte(content), 0o600); err != nil {
+		return fmt.Errorf("writing changelog file %s: %w", opts.outputPath, err)
 	}
 
-	slog.Info("changelog generated", slog.String("file", outputPath), slog.String("version", nextVersion))
+	slog.Info("changelog generated", slog.String("file", opts.outputPath), slog.String("version", nextVersion))
 
-	if githubRelease {
-		if err := createRelease(ctx, repo, nextVersion, content); err != nil {
-			return err
-		}
+	if opts.githubRelease {
+		return createRelease(ctx, repo, nextVersion, content)
 	}
 
 	return nil
+}
+
+func getTagMap(repo *gitrepo.Repo) (map[string]string, error) {
+	tagMap, err := repo.GetTagMap()
+	if err != nil {
+		return nil, fmt.Errorf("getting tags: %w", err)
+	}
+
+	strTagMap := make(map[string]string, len(tagMap))
+	for hash, tag := range tagMap {
+		strTagMap[hash.String()] = tag
+	}
+	return strTagMap, nil
 }
 
 func createRelease(ctx context.Context, repo *gitrepo.Repo, version, content string) error {
